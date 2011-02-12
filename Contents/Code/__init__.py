643d45c3c9e67019f7a161ea9dad7b58e3a4648a
Barkley Dawg <mike.d.meier@gmail.com>
2011-02-12 14:38:15 -0600

    fix thumbs and tidy code

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -29,42 +29,6 @@ FASHION_ICON  = ''
 
 VIDEO_PREFIX    = "/video/ctvglobemedia"
 
-#CTV_URL         = 'http://watch.ctv.ca/'
-#CTV_SEARCH      = 'http://watch.ctv.ca/AJAX/SearchResults.aspx?query=%s'
-#CTV_CLIP_LOOKUP = 'http://watch.ctv.ca/AJAX/ClipLookup.aspx?episodeid=%s'
-
-#CTVNEWS_URL         = 'http://watch.ctv.ca/news/'
-#CTVNEWS_SEARCH      = 'http://watch.ctv.ca/news/AJAX/SearchResults.aspx?query=%s'
-#CTVNEWS_CLIP_LOOKUP = 'http://watch.ctv.ca/news/AJAX/ClipLookup.aspx?episodeid=%s'
-
-#TSN_URL         = 'http://watch.tsn.ca/'
-#TSN_SEARCH      = 'http://watch.tsn.ca/AJAX/SearchResults.aspx?query=%s'
-#TSN_CLIP_LOOKUP = 'http://watch.tsn.ca/AJAX/ClipLookup.aspx?episodeid=%s'
-
-#DISCOVERY_URL         = 'http://watch.discoverychannel.ca/'
-#DISCOVERY_SEARCH      = 'http://watch.discoverychannel.ca/AJAX/SearchResults.aspx?query=%s'
-#DISCOVERY_CLIP_LOOKUP = 'http://watch.discoverychannel.ca/AJAX/ClipLookup.aspx?episodeid=%s'
-
-#COMEDY_URL         = 'http://watch.thecomedynetwork.ca/'
-#COMEDY_SEARCH      = 'http://watch.thecomedynetwork.ca/AJAX/SearchResults.aspx?query=%s'
-#COMEDY_CLIP_LOOKUP = 'http://watch.thecomedynetwork.ca/AJAX/ClipLookup.aspx?episodeid=%s'
-
-#SPACE_URL         = 'http://watch.spacecast.com/'
-#SAPCE_SEARCH      = 'http://watch.spacecast.com/AJAX/SearchResults.aspx?query=%s'
-#SPACE_CLIP_LOOKUP = 'http://watch.spacecast.com/AJAX/ClipLookup.aspx?episodeid=%s'
-
-#BRAVO_URL         = 'http://watch.bravo.ca/'
-#BRAVO_SEARCH      = 'http://watch.bravo.ca/AJAX/SearchResults.aspx?query=%s'
-#BRAVO_CLIP_LOOKUP = 'http://watch.bravo.ca/AJAX/ClipLookup.aspx?episodeid=%s'
-
-#MUCH_URL         = 'http://watch.muchmusic.com/'
-#MUCH_SEARCH      = 'http://watch.muchmusic.com/AJAX/SearchResults.aspx?query=%s'
-#MUCH_CLIP_LOOKUP = 'http://watch.muchmusic.com/AJAX/ClipLookup.aspx?episodeid=%s'
-
-#FASHION_URL         = 'http://watch.fashiontelevision.com/'
-#FASHION_SEARCH      = 'http://watch.fashiontelevision.com/AJAX/SearchResults.aspx?query=%s'
-#FASHION_CLIP_LOOKUP = 'http://watch.fashiontelevision.com/AJAX/ClipLookup.aspx?episodeid=%s'
-
 URL		= 'http://watch.%s/'
 LIBRARY_URL     = 'http://watch.%s/library/'
 SEARCH_URL	= 'http://watch.%s/AJAX/SearchResults.aspx?query=%s'
@@ -99,75 +63,6 @@ def MainMenu():
 
 ####################################################################################################
 
-#def GetURL(network):
-#  
-#  if network == 'CTV':
-#    return CTV_URL
-#  elif network == 'CTV News':
-#    return CTVNEWS_URL
-#  elif network == 'Discovery':
-#    return DISCOVERY_URL
-#  elif network == 'Comedy':
-#    return COMEDY_URL
-#  elif network == 'Bravo':
-#    return BRAVO_URL
-#  elif network == 'Space':
-#    return SPACE_URL
-#  elif network == 'MuchMusic':
-#    return MUCH_URL
-#  elif network == 'Fashion':
-#    return FASHION_URL
-#  else:
-#    return 'unknown'
-
-####################################################################################################
-
-#def GetSearchURL(network):
-#  
-#  if network == 'CTV':
-#    return CTV_SEARCH
-#  elif network == 'CTV News':
-#    return CTVNEWS_SEARCH
-#  elif network == 'Discovery':
-#    return DISCOVERY_SEARCH
-#  elif network == 'Comedy':
-#    return COMEDY_SEARCH
-#  elif network == 'Bravo':
-#    return BRAVO_SEARCH
-#  elif network == 'Space':
-#    return SPACE_SEARCH
-#  elif network == 'MuchMusic':
-#    return MUCH_SEARCH
-#  elif network == 'Fashion':
-#    return FASHION_SEARCH
-#  else:
-#    return 'unknown'
-
-####################################################################################################
-
-#def GetClipLookup(network):
-#  
-#  if network == 'CTV':
-#    return CTV_CLIP_LOOKUP
-#  elif network == 'CTV News':
-#    return CTVNEWS_CLIP_LOOKUP
-#  elif network == 'Discovery':
-#    return DISCOVERY_CLIP_LOOKUP
-#  elif network == 'Comedy':
-#    return COMEDY_CLIP_LOOKUP
-#  elif network == 'Bravo':
-#    return BRAVO_CLIP_LOOKUP
-#  elif network == 'Space':
-#    return SPACE_CLIP_LOOKUP
-#  elif network == 'MuchMusic':
-#    return MUCH_CLIP_LOOKUP
-#  elif network == 'Fashion':
-#    return FASHION_CLIP_LOOKUP
-#  else:
-#    return 'unknown'
-
-####################################################################################################
-
 def VideoMenu(sender, network):
   dir = MediaContainer(ViewGroup='List', title2=sender.itemTitle, art=sender.art)
   dir.Append(Function(DirectoryItem(GetFeatured, title="Featured"), network=network, title2="Featured"))
@@ -179,65 +74,32 @@ def VideoMenu(sender, network):
 
 def GetVideoLibrary(sender, url, level, title2):
   dir = MediaContainer(title2=title2)
-  if title2 == 'CTV Documentaries':
-    for show in HTML.ElementFromURL(url).xpath('//div[@id="Level%d"]/ul/li' % level):
-      title = None
-      url = url    
-      try: title = show.find('a').get('title')
-      except: pass
-      try: url = show.find('a').get('href')
-      except: pass
-      if level == 2:
-        #Log('Add a webvideo item')
-        try: summary =  item.find('dd[@class="Description"]' ).text 
-        except: summary ="No Summary Available" 
-        thumb =  show.xpath('//dd[@class="Thumbnail"]/a/img')[0].get('src')
-        #Log(thumb)
-        dir.Append(WebVideoItem(url, title, date="", summary=summary, thumb=Function(GetThumb, thumb)))    
-      else:
-        dir.Append(Function(DirectoryItem(GetVideoLibrary, title, thumb=R('icon-default.png')), level=level+1, url=url, title2=title))
-  elif title2 == 'CTV Movie':
-    for show in HTML.ElementFromURL(url).xpath('//div[@id="Level%d"]/ul/li' % level):
-      title = None
-      url = url    
-      try: title = show.find('a').get('title')
-      except: pass
-      try: url = show.find('a').get('href')
-      except: pass
-      if level == 2:
-        #Log('Add a webvideo item')
-        try: summary =  item.find('dd[@class="Description"]' ).text 
-        except: summary ="No Summary Available" 
-        thumb =  show.xpath('//dd[@class="Thumbnail"]/a/img')[0].get('src')
-        #thumb = thumb.replace('80/60', '512/384')
-        #Log(thumb)
-        dir.Append(WebVideoItem(url, title, date="", summary=summary, thumb=thumb))    
-      else:
-        dir.Append(Function(DirectoryItem(GetVideoLibrary, title, thumb=R('icon-default.png')), level=level+1, url=url, title2=title))
-  else:
-    for show in HTML.ElementFromURL(url).xpath('//div[@id="Level%d"]/ul/li' % level):
-      title = None
-      url = url    
-      try: title = show.find('a').get('title')
-      except: pass
-      try: url = show.find('a').get('href')
-      except: pass
-      playable = ''
-      try: playable = show.xpath('.//dd[@class="Play"]/a')[0].get('title')
-      except: pass
-      if playable:
-        #Log('Add a webvideo item')
-        try: title = show.xpath('.//dl[@class="Item"]/dt/a')[0].text
-        except:pass
-        #Log(title)
-        try: summary =  show.xpath('.//dd[@class="Description"]')[0].text 
-        except: summary ="No Summary Available" 
-        thumb =  show.xpath('//dd[@class="Thumbnail"]/a/img')[0].get('src')
-        #thumb = thumb.replace('80/60', '512/384')
-        #Log(thumb)
-        dir.Append(WebVideoItem(url, title, date="", summary=summary, thumb=thumb))    
-      else:
-        dir.Append(Function(DirectoryItem(GetVideoLibrary, title, thumb=R('icon-default.png')), level=level+1, url=url, title2=title))
+  for show in HTML.ElementFromURL(url).xpath('//div[@id="Level%d"]/ul/li' % level):
+    title = None
+    url = url    
+    try: title = show.find('a').get('title')
+    except: pass
+    try: url = show.find('a').get('href')
+    except: pass
+    playable = ''
+    try: playable = show.xpath('.//dd[@class="Play"]/a')[0].get('title')
+    except: pass
+    if playable:
+      #Log('Add a webvideo item')
+      try: title = show.xpath('.//dl[@class="Item"]/dt/a')[0].text
+      except:pass
+      #Log(title)
+      try: summary =  show.xpath('.//dd[@class="Description"]')[0].text 
+      except: summary ="No Summary Available"
+      try:
+        thumb =  show.xpath('.//dd[@class="Thumbnail"]/a/img')[0].get('src')
+        thumb = thumb.replace('80/60', '512/384')
+      except:
+        thumb =R(ICON)
+      #Log(thumb)
+      dir.Append(WebVideoItem(url, title, date="", summary=summary, thumb=thumb))    
+    else:
+      dir.Append(Function(DirectoryItem(GetVideoLibrary, title, thumb=R('icon-default.png')), level=level+1, url=url, title2=title))
   return dir
 
 ####################################################################################################
@@ -253,7 +115,7 @@ def GetFeatured(sender, network, title2):
       try: summary = item.find('dd[@class="Description"]').text
       except: summary = "No Summary Available" 
       thumb =  item.find('dd[@class="Thumbnail"]/a/img').get('src')
-      #thumb = thumb.replace('80/60', '512/384')
+      thumb = thumb.replace('80/60', '512/384')
       #Log(thumb)
       dir.Append(WebVideoItem(url, title, date="", summary=summary, thumb=thumb))    
     except: pass    
@@ -291,7 +153,7 @@ def Search(sender, network, query):
         except: summary = "No Description Available"
         #Log(summary)
         thumb = result.find('dl/dd[@class="SearchThumbnail"]/a/img').get('src')
-        #thumb = thumb.replace('80/60', '512/384')
+        thumb = thumb.replace('80/60', '512/384')
         #Log(thumb)
         episodeId = result.xpath('./dl/dd[@class="PlayNow"]/a')[0].get('href')
         #Log(episodeId)
@@ -303,18 +165,6 @@ def Search(sender, network, query):
       except: pass      
   return dir
 
-################################################################################
-
-def GetThumb(url):
-    '''A function to return thumbs.'''
-    url = url.replace('80/60', '512/384')
-    #Log(url)
-    try:
-        data = HTTP.Request(url, cacheTime=CACHE_1MONTH)
-        return DataObject(data, 'image/jpeg')
-    except:
-        return Redirect(R(ICON))
-        
 ################################################################################
 
 
